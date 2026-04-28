5c982eca350a61bc11ce1181ca636eb39b226cc3
ONDO ABAGHE Rooly Marvin <rollymarvinondoabaghe@gmail.com>
2026-04-28 13:54:58 +0100

    To add 10-add.c  to the 0x02-functions_nested_loops project

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include"main.h"
+/**
+ * add - Adds two numbers and return the result
+ *
+ * @a (int): First integer
+ * @a (int): Second integer
+ * Return: An integer as a result
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
