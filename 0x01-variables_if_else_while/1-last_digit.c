d329de6f02f2be9eb8225f939d317292d8d0d5cf
ONDO ABAGHE Rooly Marvin <rollymarvinondoabaghe@gmail.com>
2026-05-01 08:20:59 +0100

    To add the 0x01-variables_if_else_while project's files

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Determines either greater than 5, is less than 6, or is 0
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n, last;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	/* Here is the last digit of the number */
+	last = n % 10;
+	if (last > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, last);
+	else if (last == 0)
+		printf("Last digit of %d is %d and is 0\n", n, last);
+	else if (last < 6 && last != 0)
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
+
+	return 0;
+}
+
+
