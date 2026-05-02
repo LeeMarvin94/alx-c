8fc6abd578d814a4d17fb1e9bb15accb1dd9104f
ONDO ABAGHE Rooly Marvin <rollymarvinondoabaghe@gmail.com>
2026-05-02 18:16:48 +0100

    To add the 3-print_alphabets.c task to the second project

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include"main.h"
+/**
+ * main - Prints the alphabet in both lowercase and uppercase
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 97; c < 123; c++)
+	{
+		putchar(c);
+	}
+	for (c = 65; c < 91 ; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return 0;
+}
