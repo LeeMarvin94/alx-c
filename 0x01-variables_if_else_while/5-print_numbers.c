47a928d2295efbf22a451f6e69ec65b272ee876f
ONDO ABAGHE Rooly Marvin <rollymarvinondoabaghe@gmail.com>
2026-05-09 06:26:30 +0100

    To add the 5-print_numbers.c to the Number projects

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include"main.h"
+/**
+ * main - Prints all the digits starting from 0 to 9
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 48; c <= 57; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
+
